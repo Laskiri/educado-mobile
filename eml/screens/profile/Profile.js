946222524c3c5ba1c72609ae1a06ac0cfd919400
MBryder <mbryde22@student.aau.dk>
2024-10-29 13:58:41 +0100

    Revert "frontend progress"
    This reverts commit 5cccff281528d57a2dd0c0b798d3fe944e13c29b.

--- eml/screens/profile/Profile.js
+++ eml/screens/profile/Profile.js
@@ -1,4 +1,3 @@
-// ProfileComponent.jsx
 import React, { useEffect, useState, useCallback } from 'react';
 import {
 	View,
@@ -6,16 +5,20 @@ import {
 	ScrollView,
 } from 'react-native';
 import LogOutButton from '../../components/profile/LogOutButton';
-import ProfileNavigationButton from '../../components/profile/ProfileNavigationButton';
+import ProfileNavigationButton from '../../components/profile/ProfileNavigationButton.js';
 import UserInfo from '../../components/profile/UserInfo';
 import { useNavigation } from '@react-navigation/native';
-import { getUserInfo, getStudentInfo } from '../../services/StorageService';
+import { getUserInfo } from '../../services/StorageService';
 import errorSwitch from '../../components/general/errorSwitch';
 import ShowAlert from '../../components/general/ShowAlert';
+import { getStudentInfo } from '../../services/StorageService';
 import ProfileStatsBox from '../../components/profile/ProfileStatsBox';
 import { useFocusEffect } from '@react-navigation/native';
-import ChatBotModal from '../../components/chatBot';
 
+/**
+ * Profile screen
+ * @returns {React.Element} Component for the profile screen
+ */
 export default function ProfileComponent() {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
@@ -25,9 +28,6 @@ export default function ProfileComponent() {
 	const [studentLevel, setStudentLevel] = useState(0);
 	const [levelProgress, setLevelProgress] = useState(0);
 	const [totalPoints, setTotalPoints] = useState(0);
-
-	// State to control the ChatBot modal visibility
-	const [chatBotModalVisible, setChatBotModalVisible] = useState(false);
 
 	useEffect(() => {
 		const getInfo = navigation.addListener('focus', () => {
@@ -40,9 +40,12 @@ export default function ProfileComponent() {
 		const pointsForPreviousLevel = (student.level - 1) * 100;
 		const pointsForNextLevel = student.level * 100;
 
-		return ((student.points - pointsForPreviousLevel) / (pointsForNextLevel - pointsForPreviousLevel)) * 100;
+		return ((student.points - pointsForPreviousLevel)/(pointsForNextLevel - pointsForPreviousLevel)) * 100;
 	};
 
+	/**
+  * Fetches the user's profile from local storage
+  */ 
 	const getProfile = async () => {
 		try {
 			const fetchedProfile = await getUserInfo();
@@ -71,7 +74,7 @@ export default function ProfileComponent() {
 					console.error('Error fetching profile:', error);
 				}
 			};
-
+	
 			runAsyncFunction();
 		}, [])
 	);
@@ -82,38 +85,27 @@ export default function ProfileComponent() {
 		setTotalPoints(studentInfo.points);
 		setLevelProgress(getLevelProgress(studentInfo));
 	};
-
+  
 	useEffect(() => {
 		fetchStudentProfile();
 	}, []);
 
 	return (
-		<SafeAreaView className="bg-secondary">
-			<ScrollView className="flex flex-col">
+		<SafeAreaView className='bg-secondary'>
+			<ScrollView className='flex flex-col'>
 				<View className="flex-1 justify-start pt-[20%] h-screen">
 					<UserInfo firstName={firstName} lastName={lastName} email={email} points={totalPoints} photo={photo}></UserInfo>
 					<ProfileStatsBox studentLevel={studentLevel} levelProgress={levelProgress} />
-					<ProfileNavigationButton
-						label="Editar perfil"
-						testId={'editProfileNav'}
-						onPress={() => navigation.navigate('EditProfile')}
-					/>
-					<ProfileNavigationButton
-						label="ChatBot"
-						testId={'chatBotNav'}
-						onPress={() => setChatBotModalVisible(true)}
-					/>
-					<View className="flex flex-row pb-4">
-						<LogOutButton testID="logoutBtn" />
+					<ProfileNavigationButton label='Editar perfil' testId={'editProfileNav'} onPress={() => navigation.navigate('EditProfile')}></ProfileNavigationButton>
+					{/* The certificate page is created and works, it is only commented out to get it approved on play store
+						<ProfileNavigationButton label='Certificados' onPress={() => navigation.navigate('CertificateStack')}></ProfileNavigationButton>*/}
+					{/* Download page is not implemented yet. However, download works and can be accessed on home page when offline
+					<ProfileNavigationButton label='Download'></ProfileNavigationButton>*/}
+					<View className='flex flex-row pb-4'>
+						<LogOutButton testID='logoutBtn'></LogOutButton>
 					</View>
 				</View>
 			</ScrollView>
-
-			{/* Render the ChatBotModal */}
-			<ChatBotModal
-				visible={chatBotModalVisible}
-				onClose={() => setChatBotModalVisible(false)}
-			/>
 		</SafeAreaView>
 	);
 }
