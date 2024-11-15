6ad1cf545ef954028eaf417f441c3bd87009b796
MikailGuzel <113025194+MikailGuzel@users.noreply.github.com>
2024-11-15 14:45:15 +0100

    Continue button is added
    Continue button is added and lecture and exercise info is moved.

--- eml/screens/lectures/CompSwipeScreen.jsx
+++ eml/screens/lectures/CompSwipeScreen.jsx
@@ -1,164 +1,197 @@
+// CompSwipeScreen.js
+
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import Swiper from 'react-native-swiper';
+import PropTypes from 'prop-types';
+import Text from '../../components/general/Text';
 import ProgressTopBar from './ProgressTopBar';
 import LectureScreen from './LectureScreen';
-import tailwindConfig from '../../tailwind.config';
 import ExerciseScreen from '../excercise/ExerciseScreen';
+import tailwindConfig from '../../tailwind.config';
 import { completeComponent, findIndexOfUncompletedComp } from '../../services/utilityFunctions';
-import {getComponentList, getStudentInfo} from '../../services/StorageService';
-import PropTypes from 'prop-types';
-
-/* 
-Description: 	This screen is displayed when the student is doing a component.
-				It displays the component, which can be a lecture or an exercise.
-				When the student presses the continue button, or swipes, the next component is displayed.
-				The swiper is disabled when the student is doing an exercise.
-				The swiper starts at the first uncompleted component in the section.
-				The swiper is enabled when the student is doing a lecture.
-				The screen has a progress bar at the top, which shows the progress in the section.
-				It also shows the points the student has earned in the course.
-Dependencies:	That there exists a course object and a section object, which has components.
-Props:			- route: The route object, which contains the section object and the course object
-*/
+import { getComponentList, getStudentInfo } from '../../services/StorageService';
 
 const LectureType = {
-	TEXT: 'text',
-	VIDEO: 'video',
+    TEXT: 'text',
+    VIDEO: 'video',
 };
 
 const ComponentType = {
-	LECTURE: 'lecture',
-	EXERCISE: 'exercise',
+    LECTURE: 'lecture',
+    EXERCISE: 'exercise',
 };
 
 /**
- * when navigating to this page sectionId, parsedCourse must be passed as parameters
- * @param {} param
- * @returns 
+ * Description: 	This screen is displayed when the student is doing a component.
+ * 				It displays the component, which can be a lecture or an exercise.
+ * 				When the student presses the continue button, or swipes, the next component is displayed.
+ * 				The swiper is disabled when the student is doing an exercise.
+ * 				The swiper starts at the first uncompleted component in the section.
+ * 				The swiper is enabled when the student is doing a lecture.
+ * 				The screen has a progress bar at the top, which shows the progress in the section.
+ * 				It also shows the points the student has earned in the course.
+ * Dependencies:	That there exists a course object and a section object, which has components.
+ * Props:			- route: The route object, which contains the section object and the course object
  */
-export default function CompSwipeScreen({ route }) {
-	const { section, parsedCourse } = route.params;
-	const [loading, setLoading] = useState(true);
-	const [currentLectureType, setCurrentLectureType] = useState(LectureType.TEXT);
-	const [index, setIndex] = useState(0);
-	const [scrollEnabled, setScrollEnabled] = useState(true);
-	const [combinedLecturesAndExercises, setCombinedLecturesAndExercises] = useState([]);
-	const swiperRef = useRef(null);
-	const [resetKey, setResetKey] = useState(0);
+const CompSwipeScreen = ({ route }) => {
+    const { section, parsedCourse } = route.params;
+    const [loading, setLoading] = useState(true);
+    const [currentLectureType, setCurrentLectureType] = useState(LectureType.TEXT);
+    const [index, setIndex] = useState(0);
+    const [scrollEnabled, setScrollEnabled] = useState(true);
+    const [combinedLecturesAndExercises, setCombinedLecturesAndExercises] = useState([]);
+    const swiperRef = useRef(null);
+    const [resetKey, setResetKey] = useState(0);
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				let initialIndex = findIndexOfUncompletedComp(await getStudentInfo(), parsedCourse.courseId, section.sectionId);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const studentInfo = await getStudentInfo();
+                let initialIndex = findIndexOfUncompletedComp(studentInfo, parsedCourse.courseId, section.sectionId);
 
-				if (initialIndex === -1) {
-					initialIndex = 0;
-				}
+                if (initialIndex === -1) {
+                    initialIndex = 0;
+                }
 
-				const compList = await getComponentList(section.sectionId);
+                let compList = await getComponentList(section.sectionId);
 
-				if (compList[initialIndex].type === ComponentType.EXERCISE) {
-					setScrollEnabled(false);
-				}
+                // Remove the reverse if not needed
+                // compList = compList.reverse();
 
-				setCombinedLecturesAndExercises(compList);
-				combinedLecturesAndExercises.reverse();
-				setCurrentLectureType(compList[initialIndex]?.lectureType === LectureType.VIDEO ? LectureType.VIDEO : LectureType.TEXT);
-				setIndex(initialIndex);
-				setLoading(false);
-			} catch (error) {
-				setLoading(true);
-			}
-		}
+                if (compList[initialIndex].type === ComponentType.EXERCISE) {
+                    setScrollEnabled(false);
+                }
 
-		fetchData();
-	}, [section, parsedCourse]);
+                setCombinedLecturesAndExercises(compList);
+                setCurrentLectureType(compList[initialIndex]?.lectureType === LectureType.VIDEO ? LectureType.VIDEO : LectureType.TEXT);
+                setIndex(initialIndex);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching components:', error);
+                setLoading(true);
+            }
+        };
 
+        fetchData();
+    }, [section, parsedCourse]);
 
-	const handleExerciseContinue = (isCorrect) => {
-		if (!isCorrect) {
-			// Update the section component list to move the incorrect exercise to the end
-			const currentComp = combinedLecturesAndExercises[index];
-			const updatedCombinedLecturesAndExercises = [
-				...combinedLecturesAndExercises.slice(0, index), // Elements before the current index
-				...combinedLecturesAndExercises.slice(index + 1), // Elements after the current index
-				currentComp // Add the current component to the end
-			];
-			setCombinedLecturesAndExercises(updatedCombinedLecturesAndExercises);
+    // Handler for continuing from a lecture
+    const handleLectureContinue = () => {
+        swiperRef.current.scrollBy(1, true);
+    };
 
-			// Force re-render by updating the resetKey
-			setResetKey(prevKey => prevKey + 1);
-		} else {
-			swiperRef.current.scrollBy(1, true);
-			setScrollEnabled(true);
-		}
+    const handleExerciseContinue = (isCorrect) => {
+        if (!isCorrect) {
+            // Update the section component list to move the incorrect exercise to the end
+            const currentComp = combinedLecturesAndExercises[index];
+            const updatedCombinedLecturesAndExercises = [
+                ...combinedLecturesAndExercises.slice(0, index), // Elements before the current index
+                ...combinedLecturesAndExercises.slice(index + 1), // Elements after the current index
+                currentComp // Add the current component to the end
+            ];
+            setCombinedLecturesAndExercises(updatedCombinedLecturesAndExercises);
 
-		return index === combinedLecturesAndExercises.length - 1; //True if this is last lecture/exercise
-	};
+            // Force re-render by updating the resetKey
+            setResetKey(prevKey => prevKey + 1);
+        } else {
+            swiperRef.current.scrollBy(1, true);
+            setScrollEnabled(true);
+        }
 
-	const handleIndexChange = async (_index) => {
-		const currentSlide = combinedLecturesAndExercises[_index];
+        return index === combinedLecturesAndExercises.length - 1; // True if this is the last lecture/exercise
+    };
 
-		if (currentSlide.type === ComponentType.EXERCISE) {
-			setScrollEnabled(false);
-		} else {
-			const currentLectureType = currentSlide?.lectureType === LectureType.VIDEO ? LectureType.VIDEO : LectureType.TEXT;
-			setCurrentLectureType(currentLectureType);
-		}
+    const handleIndexChange = async (_index) => {
+        const currentSlide = combinedLecturesAndExercises[_index];
 
-		if (_index > 0) {
-			const lastSlide = combinedLecturesAndExercises[_index - 1];
-			if (lastSlide.type === ComponentType.LECTURE) {
-				await completeComponent(lastSlide.component, parsedCourse.courseId, true);
-			}
-		}
-		setIndex(_index);
-	};
+        if (currentSlide.type === ComponentType.EXERCISE) {
+            setScrollEnabled(false);
+        } else {
+            const currentLectureType = currentSlide?.lectureType === LectureType.VIDEO ? LectureType.VIDEO : LectureType.TEXT;
+            setCurrentLectureType(currentLectureType);
+            setScrollEnabled(true);
+        }
 
-	if (loading || !section || !parsedCourse || !combinedLecturesAndExercises) {
-		return (
-			<View className="flex-col justify-center items-center h-screen" >
-				<ActivityIndicator size="large" color={tailwindConfig.theme.colors.primary_custom} />
-				<Text>Loading...</Text>
-			</View>
-		);
-	} else {
-		return (
-			<View className="flex-1">
-				{combinedLecturesAndExercises && (
-					<View className=" absolute top-0 z-10 w-[100%]">
-						<ProgressTopBar courseObject={parsedCourse} lectureType={currentLectureType} components={combinedLecturesAndExercises} currentIndex={index} />
-					</View>
-				)}
+        if (_index > 0) {
+            const lastSlide = combinedLecturesAndExercises[_index - 1];
+            try {
+                await completeComponent(lastSlide.component, parsedCourse.courseId, true);
+            } catch (error) {
+                console.error('Error completing component:', error);
+                // Optionally, show an error message to the user
+            }
+        }
+        setIndex(_index);
+    };
 
-				{combinedLecturesAndExercises.length > 0 && parsedCourse && index !== null && (
-					<Swiper
-						ref={swiperRef}
-						index={index}
-						onIndexChanged={(_index) => handleIndexChange(_index)}
-						loop={false}
-						showsPagination={false}
-						scrollEnabled={scrollEnabled}
-					>
-						{combinedLecturesAndExercises.map((comp, _index) => (
-							comp.type === ComponentType.LECTURE ?
-								<LectureScreen key={_index} currentIndex={index} indexCount={combinedLecturesAndExercises.length} lectureObject={comp.component} courseObject={parsedCourse} />
-								:
-								<ExerciseScreen key={resetKey} componentList={combinedLecturesAndExercises} exerciseObject={comp.component} sectionObject={section} courseObject={parsedCourse} onContinue={(isCorrect) => handleExerciseContinue(isCorrect)} />
-						))}
-					</Swiper>
-				)}
-			</View>
-		);
-	}
-}
+    if (loading || !section || !parsedCourse || !combinedLecturesAndExercises) {
+        return (
+            <View className="flex-col justify-center items-center h-screen">
+                <ActivityIndicator size="large" color={tailwindConfig.theme.colors.primary_custom} />
+                <Text>Loading...</Text>
+            </View>
+        );
+    } else {
+        return (
+            <View className="flex-1">
+                {combinedLecturesAndExercises && (
+                    <View className="absolute top-0 z-10 w-full">
+                        <ProgressTopBar
+                            courseObject={parsedCourse}
+                            lectureType={currentLectureType}
+                            components={combinedLecturesAndExercises}
+                            currentIndex={index}
+                        />
+                    </View>
+                )}
+
+                {combinedLecturesAndExercises.length > 0 && parsedCourse && index !== null && (
+                    <Swiper
+                        key={resetKey} // Add key to force re-render when resetKey changes
+                        ref={swiperRef}
+                        index={index}
+                        onIndexChanged={handleIndexChange}
+                        loop={false}
+                        showsPagination={false}
+                        scrollEnabled={scrollEnabled}
+                    >
+                        {combinedLecturesAndExercises.map((comp, _index) => (
+                            comp.type === ComponentType.LECTURE ? (
+                                <LectureScreen
+                                    key={comp.component._id || _index} // Use a unique key if available
+                                    currentIndex={_index}
+                                    indexCount={combinedLecturesAndExercises.length}
+                                    lectureObject={comp.component}
+                                    courseObject={parsedCourse}
+                                    isLastSlide={_index === combinedLecturesAndExercises.length - 1}
+                                    onContinue={handleLectureContinue} // Pass handleLectureContinue here
+                                />
+                            ) : (
+                                <ExerciseScreen
+                                    key={comp.component._id || _index} // Use a unique key if available
+                                    componentList={combinedLecturesAndExercises}
+                                    exerciseObject={comp.component}
+                                    sectionObject={section}
+                                    courseObject={parsedCourse}
+                                    onContinue={(isCorrect) => handleExerciseContinue(isCorrect)}
+                                />
+                            )
+                        ))}
+                    </Swiper>
+                )}
+            </View>
+        );
+    }
+};
 
 CompSwipeScreen.propTypes = {
-	route: PropTypes.shape({
-		params: PropTypes.shape({
-			section: PropTypes.object,
-			parsedCourse: PropTypes.object
-		}).isRequired,
-	}).isRequired,
+    route: PropTypes.shape({
+        params: PropTypes.shape({
+            section: PropTypes.object.isRequired,
+            parsedCourse: PropTypes.object.isRequired,
+        }).isRequired,
+    }).isRequired,
 };
+
+export default CompSwipeScreen;
--- eml/screens/lectures/TextImageLectureScreen.jsx
+++ eml/screens/lectures/TextImageLectureScreen.jsx
@@ -1,155 +1,150 @@
+// TextImageLectureScreen.js
+
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import Text from '../../components/general/Text';
-import { Image } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { View, TouchableOpacity, Image } from 'react-native';
 import PropTypes from 'prop-types';
-import * as StorageService from '../../services/StorageService';
+import { Icon } from '@rneui/themed';
+import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
-import StandardButton from '../../components/general/StandardButton';
+import Text from '../../components/general/Text';
+import * as StorageService from '../../services/StorageService';
 import { completeComponent, handleLastComponent } from '../../services/utilityFunctions';
 
-const TextImageLectureScreen = ({ lectureObject, courseObject, isLastSlide }) => {
-	const [imageUrl, setImageUrl] = useState(null);
-	const [paragraphs, setParagraphs] = useState(null);
-	const navigation = useNavigation();
+const TextImageLectureScreen = ({ lectureObject, courseObject, isLastSlide, onContinue }) => {
+    const [imageUrl, setImageUrl] = useState(null);
+    const [paragraphs, setParagraphs] = useState(null);
+    const navigation = useNavigation();
 
-	const handleContinue = async () => {
-		await completeComponent(lectureObject, courseObject.courseId, true);
-		handleLastComponent(lectureObject, courseObject, navigation);
-	};
+    const handleContinue = async () => {
+        try {
+            await completeComponent(lectureObject, courseObject.courseId, true);
+            if (isLastSlide) {
+                handleLastComponent(lectureObject, courseObject, navigation);
+            } else {
+                onContinue(); // Advance to the next slide
+            }
+        } catch (error) {
+            console.error('Error completing the component:', error);
+            // Optionally, show an error message to the user
+        }
+    };
 
-	useEffect(() => {
-		if (lectureObject.image) {
-			getLectureImage();
-		}
-		splitText(lectureObject.content);
-	}, []);
+    useEffect(() => {
+        if (lectureObject.image) {
+            getLectureImage();
+        }
+        splitText(lectureObject.content);
+    }, []);
 
+    const getLectureImage = async () => {
+        try {
+            const imageRes = await StorageService.fetchLectureImage(lectureObject.image, lectureObject._id);
+            setImageUrl(imageRes);
+        } catch (err) {
+            setImageUrl(null);
+        }
+    };
 
-	const getLectureImage = async () => {
-		try {
-			const imageRes = await StorageService.fetchLectureImage(lectureObject.image, lectureObject._id);
-			setImageUrl(imageRes);
-		}
-		catch (err) {
+    // Split text into paragraphs without cutting words
+    const splitText = (text) => {
+        let _paragraphs = [];
 
-			setImageUrl(null);
-		}
-	};
+        if (text.length < 250) {
+            _paragraphs.push(text);
+            setParagraphs(_paragraphs);
+            return;
+        }
 
-	//split text into paragraphs and don't cut words
-	const splitText = (text) => {
-		let _paragraphs = [];
+        const findBreakPoint = (str, start, direction = 1) => {
+            let pos = start;
+            while (pos > 0 && pos < str.length) {
+                if (str[pos] === ' ') return pos;
+                pos += direction;
+            }
+            return pos;
+        };
 
-		if (text.length < 250) {
-			_paragraphs.push(text);
-			setParagraphs(_paragraphs);
-			return;
-		}
+        if (text.length <= 250) {
+            _paragraphs.push(text);
+        } else {
+            const breakPoint1 = findBreakPoint(text, 250);
+            _paragraphs.push(text.substring(0, breakPoint1));
 
-		// Function to find the nearest space to break the string.
-		const findBreakPoint = (str, start, direction = 1) => {
-			let pos = start;
-			while (pos > 0 && pos < str.length) {
-				if (str[pos] === ' ') return pos;
-				pos += direction;
-			}
-			return pos;
-		};
+            let remainingText = text.substring(breakPoint1);
 
-		// If the text is less than 300 characters, then the first chunk should be the entire text.
-		if (text.length <= 250) {
-			_paragraphs.push(text);
-		} else {
-			// Find the nearest space to 300th character to break the string.
-			const breakPoint1 = findBreakPoint(text, 250);
-			_paragraphs.push(text.substring(0, breakPoint1));
+            while (remainingText.length > 0) {
+                const breakPoint = findBreakPoint(remainingText, 100);
+                const chunk = remainingText.substring(0, breakPoint);
+                _paragraphs.push(chunk);
+                remainingText = remainingText.substring(breakPoint);
+            }
+        }
 
-			// Remaining text to be divided into 150 character chunks.
-			let remainingText = text.substring(breakPoint1);
+        setParagraphs(_paragraphs);
+    };
 
-			while (remainingText.length > 0) {
-				const breakPoint = findBreakPoint(remainingText, 100);
-				const chunk = remainingText.substring(0, breakPoint);
-				_paragraphs.push(chunk);
-				remainingText = remainingText.substring(breakPoint);
-			}
-		}
+    return (
+        <View className="flex-1 bg-secondary px-4 pt-20">
+            <View className="flex-col mt-5 items-center">
+                <Text className="font-sans text-base text-projectGray">
+                    Nome do curso: {courseObject.title}
+                </Text>
+                <Text className="font-sans-bold text-lg text-projectBlack">
+                    {lectureObject.title}
+                </Text>
+            </View>
 
-		// Now, `_paragraphs` contains the split text.
-		setParagraphs(_paragraphs);
-	};
+            <View className="flex-1 w-full">
+                <ScrollView className="max-h-128 flex-grow mt-2 mb-4">
+                    {paragraphs && paragraphs.map((paragraph, index) => (
+                        <Text
+                            key={index}
+                            className={`text-lg pt-4 px-4 ${index === 0 ? 'text-primary_custom' : 'text-projectGray'}`}
+                        >
+                            {paragraph}
+                        </Text>
+                    ))}
+                    {imageUrl && (
+                        <View className="w-full h-[25vh] px-4 pt-8">
+                            <Image source={{ uri: imageUrl }} className="w-full h-full" />
+                        </View>
+                    )}
+                    {paragraphs && paragraphs.length > 2 && (
+                        <Text className="text-[18px] px-4 text-projectGray">
+                            {paragraphs[paragraphs.length - 1]}
+                        </Text>
+                    )}
+                </ScrollView>
+            </View>
 
-	return (
-		<View className={'absolute w-full h-full px-4 pt-20'}>
-			{/* Content */}
-			<Text className="text-center text-2xl pt-6 font-bold">BEM VINDO!</Text>
-			<ScrollView className="mt-2">
-
-
-				{
-					// Rendering all paragraphs above the image if the array has two or fewer elements
-					// If the array has more than two elements, rendering all but the last paragraph above the image
-					paragraphs && paragraphs.map((paragraph, index) => {
-						if (paragraphs.length <= 2 || index !== paragraphs.length - 1) {
-							return (
-								index == 0 ?
-									<Text key={index} className="text-lg pt-4 px-4 text-primary_custom">{paragraph}</Text>
-									:
-									<Text key={index} className="text-lg pt-4 px-4 text-projectGray">{paragraph}</Text>
-							);
-						}
-						return null;
-					})
-				}
-
-				{/* Image */}
-				{imageUrl && <View className="w-full h-[25vh] px-4 pt-8" >
-					<Image
-						source={{ uri: imageUrl }}
-						className="w-full h-full"
-					/>
-				</View>}
-				{
-					// Rendering the last paragraph below the image if the array has more than two elements
-					paragraphs && paragraphs.length > 2 &&
-        <Text className="text-[18px] px-4 text-projectGray">{paragraphs[paragraphs.length - 1]}</Text>
-				}
-			</ScrollView>
-
-			{isLastSlide ?
-				<View className="w-full items-center">
-					<View className="px-6 mb-3 w-screen">
-						<StandardButton
-							props={{
-								buttonText: 'Continuar',
-								onPress: () => {
-									handleContinue();
-								}
-							}}
-						/>
-					</View>
-				</View>
-				: null}
-
-			<View className="flex-col w-full justify-left drop-shadow-2xl mt-2 pb-4 pt-2" >
-				{/* Course name and lecturen name */}
-				<View className="w-full flex-row justify-between">
-
-					<View className="flex-col mb-8">
-						<Text className="text-projectGray" >Nome do curso: {courseObject.title}</Text>
-						<Text className="text-xl font-bold text-projectBlack" >{lectureObject.title}</Text>
-					</View>
-				</View>
-			</View>
-		</View>);
+            <View className="w-100 px-6 mb-8">
+                <TouchableOpacity
+                    className="bg-primary_custom px-10 py-4 rounded-medium flex-row items-center justify-center"
+                    onPress={handleContinue}
+                >
+                    <View className='flex-row items-center'>
+                        <Text className="text-center font-sans-bold text-body text-projectWhite">
+                            Continuar
+                        </Text>
+                        <Icon
+                            name="chevron-right"
+                            type="material"
+                            size={24}
+                            color="white"
+                            style={{ marginLeft: 8 }}
+                        />
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 TextImageLectureScreen.propTypes = {
-	lectureObject: PropTypes.object,
-	courseObject: PropTypes.object,
-	isLastSlide: PropTypes.bool
+    lectureObject: PropTypes.object.isRequired,
+    courseObject: PropTypes.object.isRequired,
+    isLastSlide: PropTypes.bool.isRequired,
+    onContinue: PropTypes.func.isRequired,
 };
 
 export default TextImageLectureScreen;
